6fa4b6df341a861d1c55031c952622165a4096b7
Aditya Mishra <108148546+adityamishra-only@users.noreply.github.com>
2023-12-31 00:19:46 +0530

    Create Exceptional handling.cpp

---
+++ Exceptional handling.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Program Started"<<endl;
+    
+    int a,b,c;
+    cout<<"Enter the value of A and B:";
+    cin>>a>>b;
+
+    c=a/b;
+    cout<<"Result: "<<c<<endl;
+cout<<"Program Ended: "<<endl;
+  return 0;
+}
+
+
+
+/*
+1 OUTPUT:
+Program Started
+Enter the value of A and B:10 
+0
+
+2 OUTPUT:
+Program Started
+Enter the value of A and B:10 5
+Result: 2
+Program Ended: 
+
+
+*/
